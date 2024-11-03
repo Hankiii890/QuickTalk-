fe11283b4d357e1e2a2220b0b167cba2fe9ae11c
Hankiii890 <shenderuk4kir@gmail.com>
2024-11-03 16:48:57 +0300

    add static directory

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routs.auth import router as auth_router
 from routs.message import router as message_router
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(auth_router)
 app.include_router(message_router)
-
