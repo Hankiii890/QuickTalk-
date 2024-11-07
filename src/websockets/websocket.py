c33f843a80b31782bfb500ea3d3f1e6d96b65e49
Hankiii_890 <shenderuk4kir@gmail.com>
2024-11-07 23:02:14 +0300

    start writing websockets

--- src/websockets/websocket.py
+++ src/websockets/websocket.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
-from src.websockets import websocket_manager as manager
-
+from .websocket_manager import manager
 
 
 routs = APIRouter()
@@ -14,6 +13,8 @@ async def websockets_endpoints(websocket: WebSocket, user_id: id):
         while True:
             data = await websocket.receive_text()
             # логика обработки сообщений
-            await manager.send_personal_message(f'You wrote: {data}', f"- {user_id}")
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{user_id} say: {data}")
     except WebSocketDisconnect:
-        manager.Disconect(user_id)
+        manager.disconnect(websocket)
+        await manager.broadcast(f'Client #{user_id} left this chat')
