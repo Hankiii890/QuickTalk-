415a30f1ba5d3ebf738942c27127a39b8096a674
Hankiii_890 <shenderuk4kir@gmail.com>
2024-11-08 13:40:32 +0300

    new rout websocket

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routs.auth import router as auth_router
 from routs.message import router as message_router
+from routs.websocket import routs as websocket_router
 
 app = FastAPI()
 
@@ -9,3 +10,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth_router)
 app.include_router(message_router)
+app.include_router(websocket_router)
