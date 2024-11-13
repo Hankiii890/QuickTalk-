b10d577d5e00eb5d00b5d3b26a6cf7b578fb9097
Hankiii_890 <shenderuk4kir@gmail.com>
2024-11-13 21:47:30 +0300

    remade manager

--- src/routs/websocket_manager.py
+++ src/routs/websocket_manager.py
@@ -3,20 +3,23 @@ from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: dict[int, WebSocket] = {}  # Используем словарь для хранения соединений по user_id
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, user_id: int, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[user_id] = websocket  # Сохраняем соединение по user_id
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, user_id: int):
+        if user_id in self.active_connections:
+            del self.active_connections[user_id]  # Удаляем соединение по user_id
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def send_personal_message(self, message: str, user_id: int):
+        websocket = self.active_connections.get(user_id)
+        if websocket:
+            await websocket.send_text(message)
 
     async def broadcast(self, message: str):
-        for connection in self.active_connections:
+        for connection in self.active_connections.values():
             await connection.send_text(message)
 
 
